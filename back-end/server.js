bd0b560c6c16d95903afc7e3b5fcbb2b34d0aaa8
Peter Berry <pwcberry@gmail.com>
2025-08-06 15:56:39 +1000

    Add back-end server

--- back-end/server.js
+++ back-end/server.js
@@ -1,2 +1,32 @@
 import { createServer } from "node:http";
-import { MongoClient } from "mongodb";
+import {MongoClient} from "mongodb";
+
+async function main() {
+  const databaseClient = new MongoClient("mongodb://localhost:10000");
+
+  try {
+    await databaseClient.connect();
+    console.log("Database Connected!");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+
+  const server = createServer(
+    async(req, res) => {
+      const db = databaseClient.db("ch02");
+      const users = db.collection("users");
+
+      const usersList = await users.find().toArray();
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(usersList));
+    }
+  );
+
+  server.listen(8080, "localhost", () => {
+    console.log("Server listening on port 8080");
+  });
+}
+
+await main();
