f1a669e0344535e532b01e0db68672a4ebc883f3
Peter Berry <pwcberry@gmail.com>
2025-08-21 07:41:29 +1000

    Add functions to list posts

--- back-end/src/services/posts.js
+++ back-end/src/services/posts.js
@@ -1,6 +1,21 @@
 import { Post } from "../db/models/post.js";
 
+async function listPosts(query = {}, { sortBy = "createdAt", sortOrder = "descending" } = {}) {
+  return Post.find(query).sort({ [sortBy]: sortOrder });
+}
+
 export async function createPost({ title, author, contents, tags }) {
   const post = new Post({ title, author, contents, tags });
   return await post.save();
+}
+export async function listAllPosts(options) {
+  return await listPosts({}, options);
+}
+
+export async function listPostsByAuthor(author, options) {
+  return await listPosts({ author }, options);
+}
+
+export async function listPostsByTag(tags, options) {
+  return await listPosts({ tags }, options);
 }
--- back-end/test/services/posts.test.js
+++ back-end/test/services/posts.test.js
@@ -1,9 +1,31 @@
 import mongoose from "mongoose";
-import { describe, expect, test } from "@jest/globals";
-import { createPost } from "../../src/services/posts.js";
+import { describe, expect, test, beforeEach } from "@jest/globals";
+import { createPost, listAllPosts, listPostsByAuthor, listPostsByTag } from "../../src/services/posts.js";
 import { Post } from "../../src/db/models/post.js";
 
+const samplePosts = [
+  { title: "Learning Redux", author: "Daniel Bugl", tags: ["redux"] },
+  { title: "Learn React Hooks", author: "Daniel Bugl", tags: ["react"] },
+  {
+    title: "Full-Stack React Projects",
+    author: "Daniel Bugl",
+    tags: ["react", "nodejs"],
+  },
+  { title: "Guide to TypeScript" },
+];
+
 describe("Posts", () => {
+  let createdSamplePosts = [];
+
+  beforeEach(async () => {
+    await Post.deleteMany({});
+    createdSamplePosts = [];
+    for (const post of samplePosts) {
+      const createdPost = new Post(post);
+      createdSamplePosts.push(await createdPost.save());
+    }
+  });
+
   describe("create", () => {
     test("with all parameters: it should succeed", async () => {
       const post = {
@@ -14,7 +36,6 @@ describe("Posts", () => {
       };
 
       const createdPost = await createPost(post);
-
       expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId);
 
       const foundPost = await Post.findById(createdPost._id);
@@ -45,5 +66,33 @@ describe("Posts", () => {
       const createdPost = await createPost(post);
       expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId);
     });
+  });
+
+  describe("list", () => {
+    test("list all posts: it should succeed", async () => {
+      const posts = await listAllPosts();
+      expect(posts.length).toEqual(createdSamplePosts.length);
+    });
+    test("list all posts by default sorted by 'createdAt' descending: it should succeed", async () => {
+      const posts = await listAllPosts();
+      const sortedSamplePosts = createdSamplePosts.sort((a, b) => b.createdAt - a.createdAt);
+
+      // We cannot directly compare lists of documents returned by Mongoose because the documents will have hidden metadata
+      // that Jest will attempt to compare
+      expect(posts.map((p) => p.createdAt)).toEqual(sortedSamplePosts.map((p) => p.createdAt));
+    });
+  });
+  test("list all posts by 'updatedAt' ascending: it should succeed", async () => {
+    const posts = await listAllPosts({ sortBy: "updatedAt", sortOrder: "ascending" });
+    const sortedSamplePosts = createdSamplePosts.sort((a, b) => a.updatedAt - b.updatedAt);
+    expect(posts.map((p) => p.updatedAt)).toEqual(sortedSamplePosts.map((p) => p.updatedAt));
+  });
+  test("list posts filtered by author: it should succeed", async () => {
+    const posts = await listPostsByAuthor("Daniel Bugl");
+    expect(posts.length).toBe(3);
+  });
+  test("list posts filtered by tag: it should succeed", async () => {
+    const posts = await listPostsByTag("nodejs");
+    expect(posts.length).toBe(1);
   });
 });
