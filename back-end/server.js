037758655b7dbfdd500d8b79c0148d066064f874
Peter Berry <pwcberry@gmail.com>
2025-08-21 17:24:46 +1000

    Initial work for Express App

--- back-end/server.js
+++ back-end/server.js
@@ -1,50 +1,7 @@
-import { initDatabase } from "./src/db/init.js";
-import { Post } from "./src/db/models/post.js";
+import app from "./src/app.js";
 
-await initDatabase();
+const PORT = process.env.PORT ?? 3000;
 
-const post = new Post({
-  title: "Hello MongoDB!",
-  author: "Peter Berry",
-  contents: "<p>This is stored as an HTML string in the Mongo Database</p>",
-  tags: ["mongoose", "MongoDB", "hello_world"]
-})
-
-await post.save();
-
-const posts = await Post.find();
-console.log(posts);
-
-
-// import { createServer } from "node:http";
-// import {MongoClient} from "mongodb";
-//
-// async function main() {
-//   const databaseClient = new MongoClient("mongodb://localhost:10000");
-//
-//   try {
-//     await databaseClient.connect();
-//     console.log("Database Connected!");
-//   } catch (error) {
-//     console.error("Error connecting to database:", error);
-//   }
-//
-//   const server = createServer(
-//     async(req, res) => {
-//       const db = databaseClient.db("ch02");
-//       const users = db.collection("users");
-//
-//       const usersList = await users.find().toArray();
-//
-//       res.statusCode = 200;
-//       res.setHeader("Content-Type", "application/json");
-//       res.end(JSON.stringify(usersList));
-//     }
-//   );
-//
-//   server.listen(8080, "localhost", () => {
-//     console.log("Server listening on port 8080");
-//   });
-// }
-//
-// await main();
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
