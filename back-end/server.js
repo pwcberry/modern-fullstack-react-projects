9f2ab31583995b07c8d7cc48aae08ee86021ccb4
Peter Berry <pberry@australianunity.com.au>
2026-06-04 13:30:04 +1000

    Update MongoDB docker image

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 import app from "./src/app.js";
 import { initDatabase } from "./src/db/init.js";
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 4001;
 
 async function main() {
   await initDatabase();
