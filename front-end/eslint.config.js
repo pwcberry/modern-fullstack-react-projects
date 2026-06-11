c573cb8b07ae08d48266431f2bbcc544ed3b7cdc
Peter Berry <pwcberry@gmail.com>
2026-06-11 21:07:16 +1000

    Make the front-end TypeScript

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -1,25 +1,32 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import stylistic from "@stylistic/eslint-plugin";
+import {defineConfig, globalIgnores} from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx}"],
-    extends: [js.configs.recommended, reactHooks.configs["recommended-latest"], reactRefresh.configs.vite],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      stylistic.configs.customize({
+        indent: 2,
+        quotes: "double",
+        semi: true,
+        jsx: true,
+      })
+    ],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
     },
     rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-unused-vars": ["error", {varsIgnorePattern: "^[A-Z_]"}],
     },
   },
-]);
+])
---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Modern Full-stack React Development</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
