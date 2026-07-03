136936a0a2a869cfb18d1de1a572a5091bd2eb95
Peter Berry <pwcberry@gmail.com>
2026-07-03 13:40:34 +1000

    Add CreatePost component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,8 @@
-import Post from "./component/Post.tsx";
+import CreatePost from "./component/CreatePost.tsx";
 
 function App() {
   return (
-    <>
-      <main>
-        <h1>Modern Full-stack React Development</h1>
-        <div>
-          <Post title="Full-stack React Projects" contents="I'd rather write SPAs with Vue or Svelte." author="Peter Berry" />
-        </div>
-      </main>
-    </>
+    <CreatePost />
   );
 }
 
